26cf9f8e39f0f5c35235f271f96c21730b6e179a
Prince Raj <114799883+theprince09@users.noreply.github.com>
2025-03-11 11:38:44 +0530

    Add files via upload

---
+++ 03_conversionType.js
@@ -0,0 +1,29 @@
+let score = "33";
+let score2 = null;
+let score3 = "null";
+
+
+console.log(typeof(score));
+console.log(typeof(score2));
+
+let num = Number(score);
+let num2 = Number(score3);
+console.log(num);
+console.log(typeof(num));
+console.log(num2);
+console.log(typeof(num2));
+
+// "33" => number
+// "0" => false
+// "hitesh" => true
+
+let loggedIn = 1;
+
+let boollog = Boolean(loggedIn);
+console.log(boollog);
+console.log(typeof(boollog));
+
+let someNumber = 33;
+let someString = String(someNumber);
+console.log(someString);
+console.log(typeof(someString));
