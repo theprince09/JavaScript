fdbe87f8aca4454287d1ca7c07f571d69a5c088b
Prince Raj <114799883+theprince09@users.noreply.github.com>
2025-03-11 12:31:46 +0530

    Add files via upload

---
+++ 05_String.js
@@ -0,0 +1,26 @@
+const name = "Prince";
+const count = 50;
+
+console.log(`Hey my name is: ${name} and my repoCount is: ${count}`);
+
+const game = new String('hehehheh');
+console.log(game[0]);
+console.log(game.length);
+console.log(game.toLowerCase());
+console.log(game.toUpperCase());
+console.log(game.charAt(5));
+console.log(game.indexOf('t'));
+
+
+const newStirng = game.substring(0,4);
+console.log(newStirng);
+const slice = game.slice(0,4);
+console.log(slice);
+
+const temo = "          Prince            ";
+console.log(temo);
+console.log(temo.trim());
+
+const url = "htpps://lpu.in";
+console.log(url);
+console.log(url.replace('lpu','lovely'));
