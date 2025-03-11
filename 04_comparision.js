aac5bbd8c3a0b8ddf0a722de1055a4d87e49db66
Prince Raj <114799883+theprince09@users.noreply.github.com>
2025-03-11 11:50:09 +0530

    Add files via upload

---
+++ 04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+console.log("2">1);
+console.log("02">=1);
+console.log(null<1);
+
+console.log(null<=1);
+console.log(null==1);
+console.log(undefined==0);
+
+
+// === STRICT CHECK  it check data and data types also
+
+console.log("2" === 2);
