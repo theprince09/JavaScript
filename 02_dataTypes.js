7388c9394677899bddbd8b7c4693b2e89cdd654c
Prince Raj <114799883+theprince09@users.noreply.github.com>
2025-03-11 11:30:53 +0530

    Add files via upload

---
+++ 02_dataTypes.js
@@ -0,0 +1,26 @@
+//alert("Hello");
+
+console.log(3+3);
+console.log("Lovely");
+
+let name = "hitesh"
+let age = 18;
+let isLogged = false;
+let state;
+
+
+
+// number => 2 to power 53
+//bigInt
+// String => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+
+// object 
+// undefined is TYPE
+// but NULL is Object
+
+console.log(typeof(isLogged));
