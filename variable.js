858fcb25d9c08d22917f848ff8f29243227151f9
Prince Raj <114799883+theprince09@users.noreply.github.com>
2025-03-11 11:17:07 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,15 @@
+const accountId = 14445;
+let accountEmail = "princeraj@gmail.com";
+var accountpassword = "12345";
+accountcity = "Phagwara";
+let acc;
+
+//accountId = 5; NOT ALLOWED
+
+accountEmail = "change@gmail.com";
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountpassword);
+console.log(accountcity);
+console.log(acc);
